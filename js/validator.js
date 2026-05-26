7b58342de2b942eb0d150514244ab2b949a5075e
Prerana Poddar <preranapoddar1503@gmail.com>
2026-05-26 23:05:57 +0530

    Add files via upload

---
+++ js/validator.js
@@ -0,0 +1,13 @@
+function isValidEmail(email) {
+    const emailRegex =
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    return emailRegex.test(email);
+}
+
+function isValidPassword(password) {
+    const passwordRegex =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&^#()_\-+=])[A-Za-z\d@$!%*?&^#()_\-+=]{8,}$/;
+
+    return passwordRegex.test(password);
+}
